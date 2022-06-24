d06624f11e4ef035c69c8c0893913ee82b9dfa37
Verd1ne <76729013+Verd1ne@users.noreply.github.com>
2022-06-24 11:39:21 +0700

    Responsive text

--- smakone-website/src/Component/Content/ContentHeader.js
+++ smakone-website/src/Component/Content/ContentHeader.js
@@ -6,15 +6,15 @@ const ContentHeader = (props) => {
             <div className='container justify-center'>
                 <div className='pt-10 justify-center text-center'>
                     <h2 className='text-xl font-semibold'>{ props.category }</h2>
-                    <h1 className='text-6xl font-black'>{ props.title }</h1>
+                    <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black'>{ props.title }</h1>
                     <h2 className='text-xl font-medium'>{ props.quote }</h2>
                     <h4 className='text-xl font-semibold pt-3'>
                         <div className='text-left px-28'>
                         { props.desc }
                         </div>
                     </h4>
-                    <div className='pt-12 px-28 text-center'>
-                        <iframe className="object-cover w-full" width="997" height="558" src="https://www.youtube.com/embed/XifGVzIuT_I" title="Welcome to SMAK ONE." frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div className='pt-12 px-12 sm:px-16 xl:px-28 text-center'>
+                        <iframe className="object-cover w-full" width="992" height="558" src="https://www.youtube.com/embed/XifGVzIuT_I" title="Welcome to SMAK ONE." frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     </div>
                 </div>
             </div>
--- smakone-website/src/Component/Content/ContentText.js
+++ smakone-website/src/Component/Content/ContentText.js
@@ -3,8 +3,8 @@ import React from 'react'
 const ContentText = (props) =>{
     return(
         <div className="container mx-auto mt-12">
-            <div className="text-xl font-bold ml-32 mt-12 text-left pr-36">{ props.title }</div>
-            <div className="text-lg ml-32 mt-2 text-left pr-36 font-medium text-gray-500">{ props.text }</div>
+            <div className="text-lg xl:text-xl font-bold ml-12 sm:ml-16 xl:ml-32 mt-12 text-left pr-12 sm:pr-16 xl:pr-36">{ props.title }</div>
+            <div className="text-lg ml-12 sm:ml-16 xl:ml-32 mt-2 text-left pr-12 sm:pr-16 xl:pr-36 font-medium text-gray-500">{ props.text }</div>
         </div>
     )
 } 
