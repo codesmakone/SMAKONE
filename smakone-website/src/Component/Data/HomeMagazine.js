b67c5d6bc040909dfbd2161ae33c8651a3ff43b2
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2022-08-01 19:15:11 +0700

    JuvunescentJuly

--- smakone-website/src/Component/Data/HomeMagazine.js
+++ smakone-website/src/Component/Data/HomeMagazine.js
@@ -1,4 +1,4 @@
-import FestiveFebruary from "../../Image/Emagz/FestiveFebruary.webp"
+import JuvunescentJuly from "../../Image/Emagz/JuvunescentJuly.webp"
 import Floraison from "../../Image/Emagz/Floraison.webp"
 import AphroditeApril from "../../Image/Emagz/AphroditeApril.webp"
 import SummerMaylodies from "../../Image/Emagz/SummerMaylodies.webp"
@@ -6,26 +6,26 @@ import SummerMaylodies from "../../Image/Emagz/SummerMaylodies.webp"
 export const HomeMagazine = [
     {
         id:1,
-        title: "Festive February",
-        img:FestiveFebruary,
-        link:"https://heyzine.com/flip-book/8f4e62c54f.html"
-    },
-    {
-        id:2,
         title: "Floraison",
         img:Floraison,
         link:"https://heyzine.com/flip-book/dc4f4f44ec.html"
     },
     {
-        id:3,
+        id:2,
         title: "Aphrodite April",
         img:AphroditeApril,
         link:"https://heyzine.com/flip-book/5c1931dac5.html"
     },
     {
-        id:4,
+        id:3,
         title: "Summer Maylodies",
         img:SummerMaylodies,
         link:"https://heyzine.com/flip-book/a5b0ed1cb6.html"
+    },
+    {
+        id: 4,
+        title: "Juvunescent July",
+        img:JuvunescentJuly,
+        link:"https://heyzine.com/flip-book/c5dcd15dd7.html"
     },
 ]
