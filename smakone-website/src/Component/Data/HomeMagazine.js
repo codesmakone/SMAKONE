a6921a5773e8436d63c986623997e3aac06866f1
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2022-11-09 18:39:26 +0700

    laskdsd

--- smakone-website/src/Component/Data/HomeMagazine.js
+++ smakone-website/src/Component/Data/HomeMagazine.js
@@ -6,26 +6,26 @@ import SummerMaylodies from "../../Image/Emagz/SummerMaylodies.webp"
 export const HomeMagazine = [
     {
         id:1,
-        title: "Aphrodite April",
-        img:AphroditeApril,
-        link:"https://heyzine.com/flip-book/5c1931dac5.html"
-    },
-    {
-        id:2,
         title: "Summer Maylodies",
         img:SummerMaylodies,
         link:"https://heyzine.com/flip-book/a5b0ed1cb6.html"
     },
     {
-        id: 3,
+        id: 2,
         title: "Juvenescent July",
         img:JuvunescentJuly,
         link:"https://heyzine.com/flip-book/c5dcd15dd7.html"
     },
     {
-        id: 4,
+        id: 3,
         title: "Inspiration Latitude",
         img:InspirationLatitude,
         link: "https://heyzine.com/flip-book/48beb98269.html"
     },
+    {
+        id:4,
+        title: "Autumn Sonata",
+        img:AutumnSonata,
+        link: "https://heyzine.com/flip-book/a71b543af5.html"
+    }
 ]
