d49782eaa661ba422175065bb4c2fc24b3a25d6b
Edward Rusli <erusli@sabarjaya.com>
2024-07-05 03:12:59 +0700

    updated july 2024

--- smakone-website/src/Component/Data/HomeMagazine.js
+++ smakone-website/src/Component/Data/HomeMagazine.js
@@ -1,47 +1,45 @@
-import BlossomLuminance from '../../Image/Emagz/BlossomLuminance.webp'
-import Cornucopia from '../../Image/Emagz/Cornucopia.webp'
-import AutumnSonata from '../../Image/Emagz/AutumnSonata.webp'
-import JubileeJuly from '../../Image/Emagz/JubileeJuly.webp'
+import ReadySetSucceed from "../../Image/Emagz/ReadySetSucceed.webp";
+import BlossomLuminance from "../../Image/Emagz/BlossomLuminance.webp";
+import Cornucopia from "../../Image/Emagz/Cornucopia.webp";
+import AutumnSonata from "../../Image/Emagz/AutumnSonata.webp";
 
 //paling kiri itu id 4, paling kanan itu id 1
 export const HomeMagazine = [
-    {
-        id:1,
-        title: "Jubilee July",
-        img:JubileeJuly,
-        link:"https://heyzine.com/flip-book/87d5fdb241.html"
-    },
-    {
-        id:2,
-        title: "Autumn Sonata",
-        img:AutumnSonata,
-        link: "https://heyzine.com/flip-book/ca5d67add9.html"
-    },
-    {
-        id:3,
-        title: "Cornucopia",
-        img:Cornucopia,
-        link:"https://heyzine.com/flip-book/f7f5a00488.html"
-    },
-    {
-        id:4,
-        title: "Blossom Luminance",
-        img:BlossomLuminance,
-        link: "https://heyzine.com/flip-book/dddac7ea5d.html"
-},
+  {
+    id: 1,
+    title: "Autumn Sonata",
+    img: AutumnSonata,
+    link: "https://heyzine.com/flip-book/ca5d67add9.html",
+  },
+  {
+    id: 2,
+    title: "Cornucopia",
+    img: Cornucopia,
+    link: "https://heyzine.com/flip-book/f7f5a00488.html",
+  },
+  {
+    id: 3,
+    title: "Blossom Luminance",
+    img: BlossomLuminance,
+    link: "https://heyzine.com/flip-book/dddac7ea5d.html",
+  },
+  {
+    id: 4,
+    title: "Ready, Set, Succeed!",
+    img: ReadySetSucceed,
+    link: "https://heyzine.com/flip-book/89dd4504be.html",
+  },
 
-    
-    
-    // {
-    //     id:3,
-    //     title: "Cornucopia",
-    //     img:Cornucopia,
-    //     link: "https://smakone.org/OSIS/Emagz/Cornucopia"
-    // },
-    // {
-    //     id:4,
-    //     title: "Blossom Luminance",
-    //     img:BlossomLuminance,
-    //     link:"https://smakone.org/OSIS/Emagz/BlossomLuminance"
-    // },
-]
+  // {
+  //     id:3,
+  //     title: "Cornucopia",
+  //     img:Cornucopia,
+  //     link: "https://smakone.org/OSIS/Emagz/Cornucopia"
+  // },
+  // {
+  //     id:4,
+  //     title: "Blossom Luminance",
+  //     img:BlossomLuminance,
+  //     link:"https://smakone.org/OSIS/Emagz/BlossomLuminance"
+  // },
+];
