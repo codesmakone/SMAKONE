46f0270ef3a1c65d1116d0edaf1101a954e9e179
Ivan <ivanaaryvidyan57@gmail.com>
2025-01-14 21:06:54 +0700

    Hasil seleksi TOS

--- smakone-website/src/Pages/Clubs/SYC/SYC.js
+++ smakone-website/src/Pages/Clubs/SYC/SYC.js
@@ -1,24 +1,21 @@
-import React from 'react' 
+import React from "react";
 
-import Nav from '../../../Component/Nav/Nav'
-import Footer from '../../../Component/Footer/Footer'
-import Header from '../../Clubs/SYC/Header'
-import Perjalanan from '../../Clubs/SYC/Perjalanan'
-import Penampilan from '../../Clubs/SYC/Penampilan'
-
-
+import Nav from "../../../Component/Nav/Nav";
+import Footer from "../../../Component/Footer/Footer";
+import Header from "../../Clubs/SYC/Header";
+import Perjalanan from "../../Clubs/SYC/Perjalanan";
+import Penampilan from "../../Clubs/SYC/Penampilan";
 
 const SYC = () => {
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <Penampilan />
+      <Perjalanan />
+      <Footer />
+    </div>
+  );
+};
 
-    return(
-            <div>
-                <Nav/>
-                <Header/>
-                <Penampilan/>
-                <Perjalanan/>
-                <Footer/>
-            </div>
-    )
-}
-
-export default SYC
+export default SYC;
