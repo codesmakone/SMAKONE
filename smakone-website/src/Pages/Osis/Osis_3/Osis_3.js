ba7da322bf5ad915978141f6d9f744add8dec551
aspekii <154879347+aspekii@users.noreply.github.com>
2025-01-22 11:43:30 +0700

    Update Program kerja bid 1-10

--- smakone-website/src/Pages/Osis/Osis_3/Osis_3.js
+++ smakone-website/src/Pages/Osis/Osis_3/Osis_3.js
@@ -16,28 +16,30 @@ const OSIS_3 = () => {
       <OsisTeam
         bidang="Bidang 3"
         desc="Bidang Pembinaan Kepribadian Unggul, Wawasan Kebangsaan, dan Bela Negara"
-        person1={<BidangSection img={Tiffany} alt='Tiffany' name='Tiffany Julian Hutagaol' desc='Hi semuaa! salam kenal nama aku Tiffany Julian Hutagaol, biasa dipanggil Tiff. Sekarang, aku berada di kelas XI MIPA. Tahun ini, aku merupakan bagian dari OSIS bidang 3. Di waktu senggang, aku suka baca novel- novel fiksi dan mabar sama teman-teman aku. Berada di OSIS merupakan suatu kesempatan yang sangat berharga, jadii jangan ragu ragu buat jadi a part of us yaa! Kalau mau tanya- tanya seputar SMUKIE atau mau curhat dan main bareng jangan ragu buat reach out ke instagram aku @tiffany.hutagaol Goodluck and jangan lupa buat have fun di SMUKIE semuanyaa!'/>}
-        person2={<BidangSection img={Anthony} alt='Anthony' name='Anthony Christian' desc='Halo semuanya 👋🏼 kenalin aku Anthony dari bidang 3!!! Cerita dikit tentang aku, lahir di Jakarta tanggal 30 November 2007. Hobi aku cover" lagu pake gitar, main badmin, dan ngegym ( walaupun gak sering tapi lumayanlah). Pelajaran favorit aku fisika sama pelajarin kamuu, wkwkww canda". Kalau ada mau nanya" hal lainn, boleh banget dm aku di IG @anthonychrist_. Salam kenal semuaa!😃'/>}
-        person3={<BidangSection img={Rayna} alt='Rayna' name='Rayna Miyesha Gunawan' desc='Halo semuaa! 大家好! Perkenalkan nama aku Rayna dari bidang 3! Aku seneng banget bisa jadi a part of this family. Fun fact tentang aku, aku nga suka (nga bisa) belajar sambil dengerin musik, dan makanan favorit aku itu apa aja yang ada kejunya! 🧀 aku suka banget makan keju (nga heran si, soalnya shio aku tikus hehe). Aku juga suka main game online lo! jadi kalau mau mabar bolee mampir ke IG aku @raynagnwn! see youu guyss'/>}
-        person4={<BidangSection img={Dimitry} alt='Dimitry' name='Dimitry Marvello' desc='Haloo! Salken semua, nama aku Dimitry Marvello biasa dipanggil Dimitry atau Dimi. Hobi aku main basket ato ga main musik. Kalo mau tau tentang aku bisa cek di ig @dimitrymarvello. Good luck guys!'/>}
+        person1={<BidangSection img={Tiffany} alt='Tiffany' name='Tiffany Julian Hutagaol' desc='Hi semuaa! salam kenal nama aku Tiffany Julian Hutagaol, biasa dipanggil Tiff. Sekarang, aku berada di kelas XI MIPA. Tahun ini, aku merupakan bagian dari OSIS bidang 3. Di waktu senggang, aku suka baca novel- novel fiksi dan mabar sama teman-teman aku. Berada di OSIS merupakan suatu kesempatan yang sangat berharga, jadii jangan ragu ragu buat jadi a part of us yaa! Kalau mau tanya- tanya seputar SMUKIE atau mau curhat dan main bareng jangan ragu buat reach out ke instagram aku @tiffany.hutagaol Goodluck and jangan lupa buat have fun di SMUKIE semuanyaa!' />}
+        person2={<BidangSection img={Anthony} alt='Anthony' name='Anthony Christian' desc='Halo semuanya 👋🏼 kenalin aku Anthony dari bidang 3!!! Cerita dikit tentang aku, lahir di Jakarta tanggal 30 November 2007. Hobi aku cover" lagu pake gitar, main badmin, dan ngegym ( walaupun gak sering tapi lumayanlah). Pelajaran favorit aku fisika sama pelajarin kamuu, wkwkww canda". Kalau ada mau nanya" hal lainn, boleh banget dm aku di IG @anthonychrist_. Salam kenal semuaa!😃' />}
+        person3={<BidangSection img={Rayna} alt='Rayna' name='Rayna Miyesha Gunawan' desc='Halo semuaa! 大家好! Perkenalkan nama aku Rayna dari bidang 3! Aku seneng banget bisa jadi a part of this family. Fun fact tentang aku, aku nga suka (nga bisa) belajar sambil dengerin musik, dan makanan favorit aku itu apa aja yang ada kejunya! 🧀 aku suka banget makan keju (nga heran si, soalnya shio aku tikus hehe). Aku juga suka main game online lo! jadi kalau mau mabar bolee mampir ke IG aku @raynagnwn! see youu guyss' />}
+        person4={<BidangSection img={Dimitry} alt='Dimitry' name='Dimitry Marvello' desc='Haloo! Salken semua, nama aku Dimitry Marvello biasa dipanggil Dimitry atau Dimi. Hobi aku main basket ato ga main musik. Kalo mau tau tentang aku bisa cek di ig @dimitrymarvello. Good luck guys!' />}
         Description={true}
         DescBid="Bidang 3 adalah bidang yang bertanggung jawab dalam kegiatan-kegiatan yang bertujuan untuk melatih dan mengembangkan kepribadian dan wawasan kebangsaan peserta didik serta menumbuhkan sikap bela negara. Pengembangan tersebut kami jalankan melalui program-program yang kami adakan seperti Perayaan-perayaan Hari Nasional, Pasukan Pengibar Bendera (Paskibra), Pramuka, dan berbagai program lainnya yang dapat menanamkan rasa cinta tanah air dalam diri peserta didik SMAK 1."
         Program={true}
         ProgramList={
           [
             //UPDATED 2024
-          'Upacara Rutin Hari Kamis: OSIS Bidang 3 memupuk rasa nasionalisme dan membangun karakter cinta terhadap bangsanya sendiri melalui upacara rutin.',
-          'Upacara Hari Nasional: menyediakan wadah bagi siswa-siswi untuk merayakan dan memperingati hari-hari penting seperti Hari Pendidikan Nasional, Hari Lahir Pancasila, Hari Kemerdekaan Indonesia, dan hari-hari penting lainnya.',
-          'Perayaan Hari Kemerdekaan Republik Indonesia: OSIS Bidang 3 mengadakan perayaan 17 Agustus menjadi ajang bagi peserta didik untuk memupuk rasa cinta tanah air dan juga mempererat rasa persatuan dan kesatuan yang ada melalui kegiatan-kegiatan yang diadakan.',
-          'Paskibra: Pasukan Pengibar Bendera bertugas pada setiap kegiatan Upacara.',
-          'Perayaan Hari Kartini: OSIS Bidang 3 membuat perayaan untuk Hari Kartini sebagai ajang bagi peserta didik untuk mengapresiasi jasa Ibu R.A Kartini.',
-          'Perayaan Hari Guru: OSIS Bidang 3 mengadakan acara Perayaan Hari Guru sebagai ajang bagi peserta didik untuk memberi penghormatan dan menunjukkan rasa terima kasih kepada para guru yang sudah berjasa bagi mereka.',
-          'K1SAH: OSIS Bidang 3 bekerja sama dengan ekstrakurikuler manga untuk membuat komik singkat setiap perayaan hari-hari besar Indonesia.',
-          'RESTORE: OSIS Bidang 3 mengadakan program untuk pergi ke salah satu destinasi wilayah yang ada di Indonesia untuk memperkenalkan budaya sosial yang ada di Indonesia kepada siswa/siswi SMAK 1 PENABUR dengan tujuan agar siswa/siswi dapat berperan aktif dalam menjelajahi dan mengenal lebih dalam mengenai budaya-budaya Indonesia.',
+            "Upacara Rutin - Upacara Bendera yang dilaksanakan setiap bulan pada hari Kamis, oleh anggota tim paskibra SMAK 1 (FlashOne)",
+            "Upacara Hari Besar - Upacara Bendera yang dilaksanakan untuk memperingati hari besar, pada bulan Mei, Agustus, Oktober, dan November",
+            "Perayaan HUT RI - Perayaan hari kemerdekaan Indonesia yang diisi dengan berbagai kegiatan untuk menjunjung tinggi bela negara, dan menyadarkan siswa akan pentingnya mengisi kemerdekaan",
+            "Perayaan Hari Guru - Pemberian tanda kasih dan acara untuk menghormati para guru yang telah mendidik murid di sekolah",
+            "Perayaan Hari Kartini - Perayaan yang diisi upacara sekaligus acara Hari Kartini dalam bentuk workshop yang menghadirkan para wanita hebat dari berbagai profesi",
+            "Paskibra (FlashOne) - Anggota Paskibra yang menjadi petugas upacara berintegritas",
+            "TOMAT (Tolong, Maaf, Terimakasih) - Konten seperti video dan poster yang bertujuan untuk mengingatkan siswa pentingnya menggunakan Tolong, Maaf, Terimakasih dalam kehidupan sehari-hari",
+            "RESTORE - Peserta didik dapat berkunjung ke salah satu destinasi wilayah yang ada di Indonesia untuk memperkenalkan budaya sosial yang ada di Indonesia kepada SMAK 1 PENABUR dengan tujuan agar dapat berperan aktif dalam menjelajahi dan mengenal lebih dalam mengenai budaya-budaya Indonesia",
+            "K1SAH - Bekerja sama dengan ekstrakurikuler manga untuk membuat komik singkat setiap perayaan hari-hari besar Indonesia",
+            "Lagu Wajib Nasional - Memutarkan lagu wajib Nasional menggunakan radio sekolah untuk menanamkan jiwa nasionalisme dan cinta tanah air peserta didik SMAK 1",
           ]
         }
         dir="/OSIS/bidang4"
-        />
+      />
     </div>
   )
 }
