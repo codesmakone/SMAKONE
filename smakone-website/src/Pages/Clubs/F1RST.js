924e8a7f327b3c6614dac2e055dc7bee528aea63
unknown <Selvenchi>
2025-07-05 18:37:40 +0700

    onemun fix

--- smakone-website/src/Pages/Clubs/F1RST.js
+++ smakone-website/src/Pages/Clubs/F1RST.js
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../../Component/Layout/KegiatanLayout'
-import f1rst from '../../Image/Clubs/F1RST.JPG'
+import Layout from "../../Component/Layout/KegiatanLayout";
+import f1rst from "../../Image/Clubs/F1RST.JPG";
 
 const F1RST = () => {
-    return(
-        <div>
-            <Layout
-                cat = "Clubs"
-                title = "F1RST"
-                quote = '“Fourth, Third, Second, F1RST!”'
-                latterPar1 = "F1RST merupakan sebuah club tari dengan genre modern dance di bawah naungan ekskul modern dance asal SMAK 1 Penabur Jakarta. Club yang di kenal dengan slogan “Fourth, Third, Second, F1RST!” ini kerap hadir untuk memeriahkan berbagai acara dengan pertunjukan tarian yang dilangsungkan di SMAK 1 seperti SmakOneCup, SAPxSNM, CNYxVals, Prom Night, BazOne, dan lain sebagainya. F1RST juga banyak menorehkan prestasi lewat perlombaan-perlombaan yang diikuti seperti Juara 1 UBS Gold Dance Competition DKI Jakarta Honda DBL 2019, Top 12 UBS Gold Dance Competition DKI Jakarta Honda DBL 2022, Juara 1 Lomba Modern Dance Santa Ursula Cup 2023, Juara 1 Lomba Modern Dance Tiara Kasih Cup 2023, dan lain sebagainya."
-                img = {f1rst}
-                alt = "f1rst"
-                latterPar2 = "Untuk info lebih lanjut mengenai club F1RST (seperti untuk ketentuan audisi), dapat mengunjungi instagram @f1rstdancecrew atau dapat menghubungi contact person yang tertera pada post."
-                dir="/Clubs/CodeOne"
-            />
-        </div>
-        
-    )
-}
+  return (
+    <div>
+      <Layout
+        cat="Clubs"
+        title="F1RST"
+        quote="“Fourth, Third, Second, F1RST!”"
+        latterPar1="F1RST merupakan sebuah club tari dengan genre modern dance di bawah naungan ekskul modern dance asal SMAK 1 Penabur Jakarta. Club yang di kenal dengan slogan “Fourth, Third, Second, F1RST!” ini kerap hadir untuk memeriahkan berbagai acara dengan pertunjukan tarian yang dilangsungkan di SMAK 1 seperti SmakOneCup, SAPxSNM, CNYxVals, Prom Night, BazOne, dan lain sebagainya. F1RST juga banyak menorehkan prestasi lewat perlombaan-perlombaan yang diikuti seperti Juara 1 UBS Gold Dance Competition DKI Jakarta Honda DBL 2019, Top 12 UBS Gold Dance Competition DKI Jakarta Honda DBL 2022, Juara 1 Lomba Modern Dance Santa Ursula Cup 2023, Juara 1 Lomba Modern Dance Tiara Kasih Cup 2023, dan lain sebagainya."
+        img={f1rst}
+        alt="f1rst"
+        latterPar2="Untuk info lebih lanjut mengenai club F1RST (seperti untuk ketentuan audisi), dapat mengunjungi instagram @f1rstdancecrew atau dapat menghubungi contact person yang tertera pada post."
+        dir="/Clubs/CodeOne"
+      />
+    </div>
+  );
+};
 
-export default F1RST
+export default F1RST;
--- smakone-website/src/Pages/Clubs/OneMUN/Main.js
+++ smakone-website/src/Pages/Clubs/OneMUN/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
           </div>
         </div>
         <img
-          className="absolute top-[0.5rem] md:top-[2rem] xl:top-[1rem] left-[50%] -translate-x-[50%] z-2 w-[15rem] sm:w-[15rem] md:w-[25rem] xl:w-[25rem]"
+          className="absolute top-[0.5rem] md:top-[2rem] xl:top-0 left-[50%] -translate-x-[50%] z-2 w-[20rem] sm:w-[21rem] md:w-[26rem] xl:w-[31rem]"
           src={Logo}
           alt="OneMUN Logo"
         />
