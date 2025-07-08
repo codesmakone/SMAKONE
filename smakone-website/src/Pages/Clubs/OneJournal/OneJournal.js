a0d2fdd6a64ae3f338ac251ced21a23703b58c13
Bernard Thimotius Turnip <joygabriel@MacBook-Pro-M3-Bernard.local>
2025-07-08 22:34:59 +0700

    Update: perbaikan file App.js, Clubpage.js, dsb

---
+++ smakone-website/src/Pages/Clubs/OneJournal/OneJournal.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Nav from "../../../Component/Nav/Nav";
+import Footer from "../../../Component/Footer/Footer";
+import Main from "./Main";
+
+const Cressendo = () => {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Cressendo;
