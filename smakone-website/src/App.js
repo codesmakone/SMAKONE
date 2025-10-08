53ce38d0bcc70256ace3b3af913ac922494689fe
FariezThePlayer <arellyus.wang@gmail.com>
2025-10-08 20:09:27 +0700

    Rename SAPxSNM to SAPxSMS and update references
    Renamed the SAPxSNM component and related files to SAPxSMS throughout the codebase. Updated all imports, routes, navigation links, and display text to reflect the new name 'Smukiez Art Performance x Smukiez Market Square'.

--- smakone-website/src/App.js
+++ smakone-website/src/App.js
@@ -9,7 +9,7 @@ import ScrollToTop from "./Component/ScrollToTop";
 import Home from "./Pages/Home/Home";
 import Soundscape from "./Pages/Kegiatan/soundscape";
 import STN from "./Pages/Kegiatan/stn";
-import SAPxSNM from "./Pages/Kegiatan/SAPxSNM/SAPxSNM";
+import SAPxSMS from "./Pages/Kegiatan/SAPxSNM/SAPxSMS";
 import Paintings from "./Pages/Kegiatan/SAPxSNM/Paintings";
 import PaintingDetails from "./Pages/Kegiatan/SAPxSNM/PaintingDetails";
 import SOC from "./Pages/Kegiatan/soc";
@@ -112,11 +112,11 @@ function App() {
         <Route path="/kegiatan/soc" element={<SOC />} />
         <Route path="/kegiatan/soundscape" element={<Soundscape />} />
         <Route path="/kegiatan/sap" element={<STN />} />
-        <Route path="/kegiatan/sapxsnm" element={<SAPxSNM />} />
-        <Route path="/kegiatan/sapxsnm/artwork" element={<Paintings />} />{" "}
+        <Route path="/kegiatan/sapxsms" element={<SAPxSMS />} />
+        <Route path="/kegiatan/sapxsms/artwork" element={<Paintings />} />{" "}
         {/* ketuker istilah artwork sama paintings pas bikin */}
         <Route
-          path="/kegiatan/sapxsnm/artwork/:id"
+          path="/kegiatan/sapxsms/artwork/:id"
           element={<PaintingDetails />}
         />
         <Route path="/kegiatan/seni" element={<Seni />} />
